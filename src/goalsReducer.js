b8474755f796d3c5cad0da0ac749c7001529756d
abduldansani <abduldansani@gmail.com>
2024-12-05 12:41:59 -0700

    goals-reducer-challange

--- src/goalsReducer.js
+++ src/goalsReducer.js
@@ -1,23 +1,49 @@
+// function goalsReducer(goals, action) {
+//     // added, completed, deleted, edited
+//     if(action.type === "added") {
+//         return [
+//             ...goals,
+//             {
+//                 id: goals.length ? goals[goals.length - 1].id + 1 : 1,
+//                 title: action.goalTitle,
+//                 done: false
+//             }
+//         ]
+//     } else if(action.type === "completed") {
+//         return goals.map(goal => goal.id === action.goalId ? { ...goal, done: !goal.done} : goal)
+//     } else if(action.type === "deleted") {
+//         return goals.filter(goal => goal.id !== action.goalId)
+//     } else if(action.type === "edited") {
+//         return goals.map(goal => goal.id === action.goalId ? {...goal, title: action.goalTitle} : goal)
+//     } else {
+//         throw Error("Unknown action: ", + action.type)
+//     }
+// }
+
 function goalsReducer(goals, action) {
-    // added, completed, deleted, edited
-    if(action.type === "added") {
-        return [
-            ...goals,
-            {
-                id: goals.length ? goals[goals.length - 1].id + 1 : 1,
-                title: action.goalTitle,
-                done: false
-            }
-        ]
-    } else if(action.type === "completed") {
-        return goals.map(goal => goal.id === action.goalId ? { ...goal, done: !goal.done} : goal)
-    } else if(action.type === "deleted") {
-        return goals.filter(goal => goal.id !== action.goalId)
-    } else if(action.type === "edited") {
-        return goals.map(goal => goal.id === action.goalId ? {...goal, title: action.goalTitle} : goal)
-    } else {
-        throw Error("Unknown action: ", + action.type)
-    }
+  switch (action.type) {
+    case "added":
+      return [
+        ...goals,
+        {
+          id: goals.length ? goals[goals.length - 1].id + 1 : 1,
+          title: action.goalTitle,
+          done: false,
+        },
+      ];
+    case "completed":
+      return goals.map((goal) =>
+        goal.id === action.goalId ? { ...goal, done: !goal.done } : goal
+      );
+    case "deleted":
+      return goals.filter((goal) => goal.id !== action.goalId);
+    case "edited":
+      return goals.map((goal) =>
+        goal.id === action.goalId ? { ...goal, title: action.goalTitle } : goal
+      );
+    default:
+      throw new Error(`Unknown action: ${action.type}`);
+  }
 }
 
-export default goalsReducer
+export default goalsReducer;
